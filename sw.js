0fbe2940fd5efffd60a41a5e850f174b47a4de2e
Michael Becker <51341849+21beckem@users.noreply.github.com>
2023-03-03 08:03:54 +0100

    removed absolute links

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
-      'https://21beckem.github.io/A2HS-test/',
-      'https://21beckem.github.io/A2HS-test/index.html',
-      'https://21beckem.github.io/A2HS-test/index.js',
-      'https://21beckem.github.io/A2HS-test/style.css',
-      'https://21beckem.github.io/A2HS-test/images/fox1.jpg',
-      'https://21beckem.github.io/A2HS-test/images/fox2.jpg',
-      'https://21beckem.github.io/A2HS-test/images/fox3.jpg',
-      'https://21beckem.github.io/A2HS-test/images/fox4.jpg',
+      '',
+      'index.html',
+      'index.js',
+      'style.css',
+      'images/fox1.jpg',
+      'images/fox2.jpg',
+      'images/fox3.jpg',
+      'images/fox4.jpg',
     ])),
   );
 });
